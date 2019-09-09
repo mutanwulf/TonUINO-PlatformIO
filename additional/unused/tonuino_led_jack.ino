de827c832805c37d7767b55ccba1d0b026d7cbaa
Marcus S <mwulf12@gmx.net>
2019-09-10 00:09:18 +0200

    sources added

--- additional/unused/tonuino_led_jack.ino
+++ additional/unused/tonuino_led_jack.ino
@@ -1,16 +1,154 @@
-#include "main.h"
+#include <DFMiniMp3.h>
+#include <EEPROM.h>
+#include <JC_Button.h>
+#include <MFRC522.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include <avr/sleep.h>
+
 /*
    _____         _____ _____ _____ _____
   |_   _|___ ___|  |  |     |   | |     |
     | | | . |   |  |  |-   -| | | |  |  |
     |_| |___|_|_|_____|_____|_|___|_____|
-    TonUINO Version 2.1 dev
+    TonUINO Version 2.1
 
     created by Thorsten Voß and licensed under GNU/GPL.
     Information and contribution at https://tonuino.de.
-    change by 2019-09 M.Schwager modified for platformio
 */
 
+// uncomment the below line to enable five button support
+//#define FIVEBUTTONS
+
+// uncomment the below line to enable jack detected max volume support
+//#define JACKDETECT
+
+// Integration von einem LED Strip oder Ring (angepasst an die Anzahl der LEDs) - zum aktivieren // in den folgenden Zeilen löschen
+#define LED_SR        // uncomment the below line to enable LED Strip and Ring support
+//#define LED_SR_Switch // Möglichkeit die LEDs über die Tasten ein- und auszuschalten
+//#define LED_SR_Powersafe // Möglichkeit die LEDs über die Tasten ein- und auszuschalten
+
+
+
+#ifdef JACKDETECT
+  // Definition des Jackdetect Pins
+  #define jdPin 8
+#endif
+
+#ifdef JACKDETECT
+  // Variablen für Prüfung des Kopfhöreranschlusses
+  int jdMaxVolume = 12 ;     // Maximale Lautstärke wenn Kopfhörer angeschlossen ist
+  int jdInitVolume = 10 ;     // Initiale Lautstärke wenn Kopfhörer angeschlossen ist
+  int jackDetectState = HIGH ;         // aktueller Status - ob Kopfhörer angeschlossen ist
+  int lastJackDetectState = HIGH;     // vorhergehender Status - ob Kopfhörer angeschlossen ist
+  int beforeJackdetectVolume = 10;     // Lautstärke bevor Kopfhörer angeschlossen wurden
+  int beforeJackdetectMaxVolume = 10;     // Maximale Lautstärke bevor Kopfhörer angeschlossen wurden
+#endif
+
+#ifdef LED_SR
+#include <Adafruit_NeoPixel.h>
+#define LED_PIN    7              // Der Pin am Arduino vom dem das Daten Signal rausgeht
+#define LED_COUNT 30              // Anzahl an LEDs im Ring oder Strip
+ 
+// Declare NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+// Zählvarbiablen
+uint16_t loopCountdown;       // Runterzählen der Loops
+uint16_t lsrLoopCountWait;    // Definierte Anzahl wieviele Loops runtergezählt werden sollen, also wie lange gewartet wird
+uint8_t animationCountdown;   // Wie oft die einmalige Animation ausgeführt wird bevor es zurück in die Hauptschleife (Animationsmodus 0) geht
+uint8_t x;
+uint8_t y;
+uint8_t z;
+uint8_t i;
+
+// Datenvarbiablen
+uint32_t lsrColorUp = strip.Color(0, 255, 0);   // Farbe wird bei Animation nächstes Lied verwendet
+uint32_t lsrColorDown = strip.Color(0, 0, 255); // Farbe wird bei Animation Lied zurück verwendet
+uint8_t currentDetectedVolume;                  // Speichern der aktuellen Lautstärke für späteren Vergleich
+uint8_t lastDetectedVolume;                     // Speichern der Lautstärke um die Animation nur ein mal zu triggern
+uint8_t volumeScope;                            // Differenz der von euch eingestellten minimalen und maximalen Lautstärke
+uint8_t volumeScopeAmount;                      // Lautstärkenwert in deinem Scope
+uint8_t currentDetectedTrack;                   // Speichern des aktuellen Tracks für späteren Vergleich
+uint8_t lastDetectedTrack;                      // Speichern des Tracks um die Animation nur ein mal zu triggern
+uint8_t lsrAnimationMode;                       // Animationsmodus - 0: Daueranimation, 1-2 einmalige Animation (als Unterbrechung zu 0)
+uint8_t lsrAnimationTrackMode;                  // Bei Animationsmodus Liedwechsel bestimmung der Farbe und Richtung
+uint32_t lsrHueCalc;                            // Zwischenspeicher einer Farbe
+uint32_t lsrColors;                             // Zwischenspeicher einer Farbe
+uint8_t lsrColorR[LED_COUNT];                   // Zwischenspeicher des Rot-Wertes für alle LEDs
+uint8_t lsrColorG[LED_COUNT];                   // Zwischenspeicher des Grün-Wertes für alle LEDs
+uint8_t lsrColorB[LED_COUNT];                   // Zwischenspeicher des Blau-Wertes für alle LEDs
+#endif
+
+#ifdef LED_SR_Switch
+bool lsrEnable = true;
+#endif
+
+#ifdef LED_SR_Powersafe
+bool lsrPSEnable = true;
+#endif
+
+
+
+static const uint32_t cardCookie = 322417479;
+
+// DFPlayer Mini
+SoftwareSerial mySoftwareSerial(2, 3); // RX, TX
+uint16_t numTracksInFolder;
+uint16_t currentTrack;
+uint16_t firstTrack;
+uint8_t queue[255];
+uint8_t volume;
+
+struct folderSettings {
+  uint8_t folder;
+  uint8_t mode;
+  uint8_t special;
+  uint8_t special2;
+};
+
+// this object stores nfc tag data
+struct nfcTagObject {
+  uint32_t cookie;
+  uint8_t version;
+  folderSettings nfcFolderSettings;
+  //  uint8_t folder;
+  //  uint8_t mode;
+  //  uint8_t special;
+  //  uint8_t special2;
+};
+
+// admin settings stored in eeprom
+struct adminSettings {
+  uint32_t cookie;
+  byte version;
+  uint8_t maxVolume;
+  uint8_t minVolume;
+  uint8_t initVolume;
+  uint8_t eq;
+  bool locked;
+  long standbyTimer;
+  bool invertVolumeButtons;
+  folderSettings shortCuts[4];
+  uint8_t adminMenuLocked;
+  uint8_t adminMenuPin[4];
+};
+
+adminSettings mySettings;
+nfcTagObject myCard;
+folderSettings *myFolder;
+unsigned long sleepAtMillis = 0;
+static uint16_t _lastTrackFinished;
+
+static void nextTrack(uint16_t track);
+uint8_t voiceMenu(int numberOfOptions, int startMessage, int messageOffset,
+                  bool preview = false, int previewFromFolder = 0, int defaultValue = 0, bool exitWithLongPress = false);
+bool isPlaying();
+bool checkTwo ( uint8_t a[], uint8_t b[] );
+void writeCard(nfcTagObject nfcTag);
+void dump_byte_array(byte * buffer, byte bufferSize);
+void adminMenu(bool fromCard = false);
+bool knownCard = false;
 
 // implement a notification class,
 // its member methods will get called
--- include/main.h
+++ include/main.h
@@ -14,222 +14,132 @@
     #include <avr/sleep.h>
 
     // uncomment the below line to enable five button support
-    //#define FIVEBUTTONS
+	#define FIVEBUTTONS
 
-    static const uint32_t cardCookie = 362375028; // new code
-    // static const uint32_t cardCookie = 322417479; //old code
+	// uncomment the below line to enable jack detected max volume support
+	//#define JACKDETECT
 
-    // DFPlayer Mini
-    SoftwareSerial mySoftwareSerial(2, 3); // RX, TX
-    uint16_t numTracksInFolder;
-    uint16_t currentTrack;
-    uint16_t firstTrack;
-    uint8_t queue[255];
-    uint8_t volume;
+	// Integration von einem LED Strip oder Ring (angepasst an die Anzahl der LEDs) - zum aktivieren // in den folgenden Zeilen löschen
+	#define LED_SR        // uncomment the below line to enable LED Strip and Ring support
+	//#define LED_SR_Switch // Möglichkeit die LEDs über die Tasten ein- und auszuschalten
+	//#define LED_SR_Powersafe // Möglichkeit die LEDs über die Tasten ein- und auszuschalten
 
-    struct folderSettings {
-    uint8_t folder;
-    uint8_t mode;
-    uint8_t special;
-    uint8_t special2;
-    };
+	#ifdef JACKDETECT
+	  // Definition des Jackdetect Pins
+	  #define jdPin 8
+	#endif
 
-    //Debugmodus mit Ausgaben
-    #define DebugModus true
+	#ifdef JACKDETECT
+	  // Variablen für Prüfung des Kopfhöreranschlusses
+	  int jdMaxVolume = 12 ;     // Maximale Lautstärke wenn Kopfhörer angeschlossen ist
+	  int jdInitVolume = 10 ;     // Initiale Lautstärke wenn Kopfhörer angeschlossen ist
+	  int jackDetectState = HIGH ;         // aktueller Status - ob Kopfhörer angeschlossen ist
+	  int lastJackDetectState = HIGH;     // vorhergehender Status - ob Kopfhörer angeschlossen ist
+	  int beforeJackdetectVolume = 10;     // Lautstärke bevor Kopfhörer angeschlossen wurden
+	  int beforeJackdetectMaxVolume = 10;     // Maximale Lautstärke bevor Kopfhörer angeschlossen wurden
+	#endif
 
-    // If use of Statusleds
-    #define useStatusLED
+	#ifdef LED_SR
+		#include <Adafruit_NeoPixel.h>
+		#define LED_PIN    7              // Der Pin am Arduino vom dem das Daten Signal rausgeht
+		#define LED_COUNT 18              // Anzahl an LEDs im Ring oder Strip
+	 
+		// Declare NeoPixel strip object:
+		Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-    // Statusled enable FastLED 
-    #ifdef useStatusLED
-        // #include "StatusLED.h" //FastLED
-        #include <FastLED.h>
-        // #include "SmartLEDTon.h"
-        #define LED_PIN     5
-        #define COLOR_ORDER GRB
-        #define LED_TYP     WS2812B
-        #define ANZAHL_LEDS    16
+		// Zählvarbiablen
+		uint16_t loopCountdown;       // Runterzählen der Loops
+		uint16_t lsrLoopCountWait;    // Definierte Anzahl wieviele Loops runtergezählt werden sollen, also wie lange gewartet wird
+		uint8_t animationCountdown;   // Wie oft die einmalige Animation ausgeführt wird bevor es zurück in die Hauptschleife (Animationsmodus 0) geht
+		uint8_t x;
+		uint8_t y;
+		uint8_t z;
+		uint8_t i;
 
-        #define BRIGHTNESS  60
-        #define FRAMES_PER_SECOND 60
+		// Datenvarbiablen
+		uint32_t lsrColorUp = strip.Color(0, 255, 0);   // Farbe wird bei Animation nächstes Lied verwendet
+		uint32_t lsrColorDown = strip.Color(0, 0, 255); // Farbe wird bei Animation Lied zurück verwendet
+		uint8_t currentDetectedVolume;                  // Speichern der aktuellen Lautstärke für späteren Vergleich
+		uint8_t lastDetectedVolume;                     // Speichern der Lautstärke um die Animation nur ein mal zu triggern
+		uint8_t volumeScope;                            // Differenz der von euch eingestellten minimalen und maximalen Lautstärke
+		uint8_t volumeScopeAmount;                      // Lautstärkenwert in deinem Scope
+		uint8_t currentDetectedTrack;                   // Speichern des aktuellen Tracks für späteren Vergleich
+		uint8_t lastDetectedTrack;                      // Speichern des Tracks um die Animation nur ein mal zu triggern
+		uint8_t lsrAnimationMode;                       // Animationsmodus - 0: Daueranimation, 1-2 einmalige Animation (als Unterbrechung zu 0)
+		uint8_t lsrAnimationTrackMode;                  // Bei Animationsmodus Liedwechsel bestimmung der Farbe und Richtung
+		uint32_t lsrHueCalc;                            // Zwischenspeicher einer Farbe
+		uint32_t lsrColors;                             // Zwischenspeicher einer Farbe
+		uint8_t lsrColorR[LED_COUNT];                   // Zwischenspeicher des Rot-Wertes für alle LEDs
+		uint8_t lsrColorG[LED_COUNT];                   // Zwischenspeicher des Grün-Wertes für alle LEDs
+		uint8_t lsrColorB[LED_COUNT];                   // Zwischenspeicher des Blau-Wertes für alle LEDs
+	#endif
 
-        CRGB leds[ANZAHL_LEDS];
-        #define COLOR_ORDER GRB   // Datasheet WS2812 : "Follow the order of GRB to sent data"
-        #define OK_COLOR CRGB::Green
-        #define KO_COLOR CRGB::Red
-        #define WARNING_COLOR CRGB::Orange
-        #define INIT_COLOR CRGB::Navy
-        #define WAIT_COLOR CRGB::LightPink
-        #define PLAY_COLOR CRGB::ForestGreen
+	#ifdef LED_SR_Switch
+		bool lsrEnable = true;
+	#endif
 
-        //StatusLEDs anhand der Anzahl der LEDs
-        #if NUM_LEDS < 17 
-            #define stsLED_1  1
-            #define stsLED_2  1
-            #define stsLED_3  2
-            #define stsLED_4  2
-            #define stsLED_5  3
-            #define stsLED_6  3
-            #define stsLED_7  4
-            #define stsLED_8  4
-            #define stsLED_9  5
-            #define stsLED_10  5
-            #define stsLED_11  6
-            #define stsLED_12  6
-            #define stsLED_13  7
-            #define stsLED_14  7
-            #define stsLED_15  8
-            #define stsLED_16  8
-            #define stsLED_17  9
-            #define stsLED_18  9
-            #define stsLED_19  10
-            #define stsLED_20  10
-            #define stsLED_21  11
-            #define stsLED_22  11
-            #define stsLED_23  12
-            #define stsLED_24  12
-            #define stsLED_25  13
-            #define stsLED_26  13
-            #define stsLED_27  14
-            #define stsLED_28  14
-            #define stsLED_29  15
-            #define stsLED_30  15
-            #define stsLED_31  16
-            #define stsLED_32  16
-        #else
-            #define stsLED_1  1
-            #define stsLED_2  2
-            #define stsLED_3  3
-            #define stsLED_4  4
-            #define stsLED_5  5
-            #define stsLED_6  6
-            #define stsLED_7  7
-            #define stsLED_8  8
-            #define stsLED_9  9
-            #define stsLED_10 10
-            #define stsLED_11  11
-            #define stsLED_12  12
-            #define stsLED_13  13
-            #define stsLED_14  14
-            #define stsLED_15  15
-            #define stsLED_16  16
-            #define stsLED_17  17
-            #define stsLED_18  18
-            #define stsLED_19  19
-            #define stsLED_20  20
-            #define stsLED_21  21
-            #define stsLED_22  22
-            #define stsLED_23  23
-            #define stsLED_24  24
-            #define stsLED_25  25
-            #define stsLED_26  26
-            #define stsLED_27  27
-            #define stsLED_28  28
-            #define stsLED_29  29
-            #define stsLED_30  30
-            #define stsLED_31  31
-            #define stsLED_32  32
-        #endif
-    #endif
+	#ifdef LED_SR_Powersafe
+		bool lsrPSEnable = true;
+	#endif
 
-    // this object stores nfc tag data
-    struct nfcTagObject {
-        uint32_t cookie;
-        uint8_t version;
-        folderSettings nfcFolderSettings;
-        //  uint8_t folder;
-        //  uint8_t mode;
-        //  uint8_t special;
-        //  uint8_t special2;
-    };
+	static const uint32_t cardCookie = 322417479;
 
-    // admin settings stored in eeprom
-    struct adminSettings {
-        uint32_t cookie;
-        byte version;
-        uint8_t maxVolume;
-        uint8_t minVolume;
-        uint8_t initVolume;
-        uint8_t eq;
-        bool locked;
-        long standbyTimer;
-        bool invertVolumeButtons;
-        folderSettings shortCuts[4];
-        uint8_t adminMenuLocked;
-        uint8_t adminMenuPin[4];
-    };
+	// DFPlayer Mini
+	SoftwareSerial mySoftwareSerial(2, 3); // RX, TX
+	uint16_t numTracksInFolder;
+	uint16_t currentTrack;
+	uint16_t firstTrack;
+	uint8_t queue[255];
+	uint8_t volume;
 
-    adminSettings mySettings;
-    nfcTagObject myCard;
-    folderSettings *myFolder;
-    unsigned long sleepAtMillis = 0;
-    static uint16_t _lastTrackFinished;
+	struct folderSettings {
+	  uint8_t folder;
+	  uint8_t mode;
+	  uint8_t special;
+	  uint8_t special2;
+	};
 
-    static void nextTrack(uint16_t track);
-    uint8_t voiceMenu(int numberOfOptions, int startMessage, int messageOffset,
-                    bool preview = false, int previewFromFolder = 0, int defaultValue = 0, bool exitWithLongPress = false);
+	// this object stores nfc tag data
+	struct nfcTagObject {
+	  uint32_t cookie;
+	  uint8_t version;
+	  folderSettings nfcFolderSettings;
+	  //  uint8_t folder;
+	  //  uint8_t mode;
+	  //  uint8_t special;
+	  //  uint8_t special2;
+	};
 
-    //additional Subs and functions
-    bool isPlaying();
-    void setupCard();
-    void resetCard();
-    bool readCard(nfcTagObject *nfcTag);
-    bool checkTwo ( uint8_t a[], uint8_t b[] );
-    void writeCard(nfcTagObject nfcTag);
-    void dump_byte_array(byte * buffer, byte bufferSize);
-    void adminMenu(bool fromCard = false);
-    void setstandbyTimer();
-    void playFolder();
-    bool askCode(uint8_t*);
-    void playShortCut(uint8_t shortCut);
-    bool setupFolder(folderSettings * theFolder);
+	// admin settings stored in eeprom
+	struct adminSettings {
+	  uint32_t cookie;
+	  byte version;
+	  uint8_t maxVolume;
+	  uint8_t minVolume;
+	  uint8_t initVolume;
+	  uint8_t eq;
+	  bool locked;
+	  long standbyTimer;
+	  bool invertVolumeButtons;
+	  folderSettings shortCuts[4];
+	  uint8_t adminMenuLocked;
+	  uint8_t adminMenuPin[4];
+	};
 
-    #ifdef useStatusLED
-        void LEDshow();
-        void switchOnLeds(int numToFill, uint32_t color);
-    #endif
+	adminSettings mySettings;
+	nfcTagObject myCard;
+	folderSettings *myFolder;
+	unsigned long sleepAtMillis = 0;
+	static uint16_t _lastTrackFinished;
 
-    bool knownCard = false;
-
-    // MFRC522
-    #define RST_PIN 9                 // Configurable, see typical pin layout above
-    #define SS_PIN 10                 // Configurable, see typical pin layout above
-    MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522
-    MFRC522::MIFARE_Key key;
-    bool successRead;
-    byte sector = 1;
-    byte blockAddr = 4;
-    byte trailerBlock = 7;
-    MFRC522::StatusCode status;
-
-    #define buttonPause A0
-    #define buttonUp A1
-    #define buttonDown A2
-    #define busyPin 4
-    #define shutdownPin 7
-    #define openAnalogPin A7
-
-    #ifdef FIVEBUTTONS
-        #define buttonFourPin A3
-        #define buttonFivePin A4
-    #endif
-
-    #define LONG_PRESS 1000
-
-    Button pauseButton(buttonPause);
-    Button upButton(buttonUp);
-    Button downButton(buttonDown);
-    #ifdef FIVEBUTTONS
-        Button buttonFour(buttonFourPin);
-        Button buttonFive(buttonFivePin);
-    #endif
-
-    bool ignorePauseButton = false;
-    bool ignoreUpButton = false;
-    bool ignoreDownButton = false;
-    #ifdef FIVEBUTTONS
-        bool ignoreButtonFour = false;
-        bool ignoreButtonFive = false;
-    #endif
+	static void nextTrack(uint16_t track);
+	uint8_t voiceMenu(int numberOfOptions, int startMessage, int messageOffset,
+					  bool preview = false, int previewFromFolder = 0, int defaultValue = 0, bool exitWithLongPress = false);
+	bool isPlaying();
+	bool checkTwo ( uint8_t a[], uint8_t b[] );
+	void writeCard(nfcTagObject nfcTag);
+	void dump_byte_array(byte * buffer, byte bufferSize);
+	void adminMenu(bool fromCard = false);
+	bool knownCard = false;
 
 #endif
